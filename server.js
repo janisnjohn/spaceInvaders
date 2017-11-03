9ee66a10ba452e4bb7222e8f2f71249c4e631cf1
Ryan Richholt <ryanrichholt@gmail.com>
2017-11-02 20:45:38 -0700

    added json parsing

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
