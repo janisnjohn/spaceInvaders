6a7cad448173fd29d2c89ab1bfbe31663b8a0ed1
janisnjohn <janisnjohn@gmail.com>
2017-11-08 13:01:33 -0700

    function test file

--- test/test.js
+++ test/test.js
@@ -1,45 +1,63 @@
 var Nightmare = require("nightmare");
 var expect = require("chai").expect;
-describe("Codecademy", function() {
+
+
+describe("spaceInvaders", function() {
   // The default tests in mocha is 2 seconds.
   // Extending it to 30 seconds to have time to load the pages
   this.timeout(30000);
-  it("should send user to the catalog page", function(done) {
+  it("should send user to the game page", function(done) {
     // ID for the login button.
     Nightmare({ show: true })
-      .goto("https://codecademy.com")
+      .goto("http://localhost:8080/")
       // Click the catalog link
-      .click("a[href='/learn/all']")
-      // Evaluate the title
+      .click("#gameCard")
+      .click("#game")
       .evaluate(function() {
-        return document.title;
+        return document.querySelector("a[href='/game']");
       })
       // Asset the title is as expected
-      .then(function(title) {
-        expect(title).to.equal("Catalog | Codecademy");
-        done();
-      });
-  });
-  it("should present a link to course catalog after login", function(done) {
-    new Nightmare({ show: true })
-      .goto("https://www.codecademy.com/login")
-      // Enter username.
-      .type("#user_login", "ResilD")
-      // Enter password.
-      .type("#login__user_password", "dummy*password")
-      // Click the login button
-      .click("#user_submit")
-      // Evaluate the following selector
-      .evaluate(function() {
-        // Assert the "learn" link can be found
-        return document.querySelector("a[href='/learn']");
-      })
       .then(function(link) {
         expect(link).to.not.equal(undefined);
         done();
       });
   });
-  it("should throw an error for fun", function() {
-    throw new Error("Failed on purpose, just to make the Mocha output more interesting.");
+  it("should send user to the leaderboard page", function(done) {
+    // ID for the login button.
+    Nightmare({ show: true })
+      .goto("http://localhost:8080/")
+      // Click the catalog link
+      .click("#leaderboard")
+      .evaluate(function() {
+        return document.querySelector("a[href='/leaderboard']");
+      })
+      // Asset the title is as expected
+      .then(function(link) {
+        expect(link).to.not.equal(undefined);
+        done();
+      });
   });
 });
+//   it("should present a link to course catalog after login", function(done) {
+//     new Nightmare({ show: true })
+//       .goto("https://www.codecademy.com/login")
+//       // Enter username.
+//       .type("#user_login", "ResilD")
+//       // Enter password.
+//       .type("#login__user_password", "dummy*password")
+//       // Click the login button
+//       .click("#user_submit")
+//       // Evaluate the following selector
+//       .evaluate(function() {
+//         // Assert the "learn" link can be found
+//         return document.querySelector("a[href='/learn']");
+//       })
+//       .then(function(link) {
+//         expect(link).to.not.equal(undefined);
+//         done();
+//       });
+//   });
+//   it("should throw an error for fun", function() {
+//     throw new Error("Failed on purpose, just to make the Mocha output more interesting.");
+//   });
+// });
