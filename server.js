3aee3d1b1f8db5007fac187adfd7dfe8b395b16d
Michael Fox <michaelscottfox1@gmail.com>
2017-11-08 01:15:49 -0700

    updatedd jawsdb url in config.json

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,4 +27,3 @@ app.use("/api/", apiRoutes);
 app.listen(port, function() {
   console.log("App listening on port " + port);
 });
-
