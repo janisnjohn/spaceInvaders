da7e87e97e4008c29b3ebb59bf98410c10d25572
Michael Fox <michaelscottfox1@gmail.com>
2017-11-07 20:33:41 -0700

    added sequelize query for leaderboard

--- controller/html-routes.js
+++ controller/html-routes.js
@@ -1,13 +1,25 @@
 // api routes for getting and posting game results
 var express = require('express');
-var db = require('../models/index.js');
+var db = require('../models');
+
 
 
 var htmlRouter = express.Router();
 
 htmlRouter.get('/leaderboard', function(request, response){
 	console.log('/leaderboard GET');
-	response.render("leaderboard");
+
+	// execute sequelize method to find records, chain promise to store data as object
+	db.Record.findAll({
+		// sort array of records descending from highest score
+		order: [['score', 'DESC'],]
+	}).then(function(data){
+		let hbsObject = { Record: data };
+		console.log(hbsObject);
+		// render leaderboard view and attach handlebarsObject
+		response.render("leaderboard", hbsObject);
+	});
+
 });
 
 htmlRouter.get('/', function(request, response){
