c1d9f04995559df468eaa6f2a04b576761a16764
John Lee <janisnjohn@gmail.com>
2017-11-08 21:53:51 -0700

    Update server.js
    changed sync to false

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var apiRoutes = require("./controller/api-routes.js");
 app.use("/", htmlRoutes);
 app.use("/api/", apiRoutes);
 
-db.sequelize.sync({force: true}).then(function(){
+db.sequelize.sync({force: false}).then(function(){
   app.listen(port, function() {
     console.log("App listening on port " + port);
   });
